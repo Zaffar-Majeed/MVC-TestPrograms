ad9e4659fc9ad25ccfd492ca70694f506151f808
zafar <zafar.rather@apexure.com>
2014-07-16 09:52:21 +0530

    First Commit

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class MessageController : Controller
+    {
+        //
+        // GET: /Message/
+
+        public String Index(String q)
+        {
+            return HttpUtility.HtmlEncode("welcome to " + q);
+        }
+
+        public String NewMsg(int id)
+        {
+            return "the value passed is : " + id;
+        }
+    }
+}
---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        //
+        // GET: /Profile/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "profile Managment";
+            ViewBag.Message = "Profile Management Page";
+            return View();
+           
+        }
+
+        public ActionResult Profile()
+        {
+            ViewBag.Title = "My Profile";
+         
+            return View();
+        }
+
+        public String ProfileEdit()
+        {
+            
+            return "Edit Profile Here";
+        }
+
+    }
+}
